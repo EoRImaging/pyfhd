e8c84bf27601c692682e75050ea2396c5905e823
Bryna Hazelton <bryna.hazelton@gmail.com>
2025-11-13 18:33:19 -0800

    fix a couple of tests that are broken for me

--- tests/test_pyfhd_tools/test_weight_invert.py
+++ tests/test_pyfhd_tools/test_weight_invert.py
@@ -35,7 +35,7 @@ def test_weight_invert_two(data_dir):
         "visibility_grid_output_result_2.npy",
     )
     result = weight_invert(weights, threshold=threshold)
-    assert np.array_equal(result, expected_result)
+    np.testing.assert_allclose(result, expected_result, rtol=0, atol=1e-11)
 
 
 def test_weight_invert_three(data_dir):
@@ -47,7 +47,7 @@ def test_weight_invert_three(data_dir):
         "visibility_grid_input_abs_3.npy",
     )
     result = weight_invert(weights, threshold=threshold, use_abs=abs)
-    assert np.array_equal(result, expected_result)
+    np.testing.assert_allclose(result, expected_result, rtol=0, atol=1e-11)
 
 
 def test_weight_invert_four(data_dir):
