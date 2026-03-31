768f7a7c5bc4174f12c98e050a7fb50cdeda32da
Kai Saddington <kaisaddington@gmail.com>
2026-03-31 19:27:43 +1100

    added option for linear scaling + percentile clipping

--- PyFHD/plotting/image.py
+++ PyFHD/plotting/image.py
@@ -10,6 +10,7 @@ def quick_image(
         logger: Logger,
         data_range=None,
         sigma_clip_level=3,
+        percentile_clip_level=1,
         xvals=None,
         yvals=None,
         xtitle=None,
@@ -24,17 +25,20 @@ def quick_image(
         eps=False,
     ):
     """
-    Create a logarithmically scaled image from a 2D array of data, with optional
-    handling for missing values and sigma clipping to determine display range.
+    Create an image from a 2D array of data, with optional handling for log scaling,
+    missing values and sigma clipping to determine display range.
     The image can be saved to a file or displayed on screen.
 
     1.  Locate missing values in data, replace with NaN and create mask for them,
         and reserve colour index 0 for them if they exist.
     2.  Determine display range for data. If not provided, apply sigma clipping
-        to log of data to determine a more representative range that is not
-        skewed by bright point sources or calibration artefacts.
-    3.  Map data values onto integer colour range [colour_min, 255] using a linear
-        stretch of the log of the data.
+        to log of data (if log is True, otherwise just data) to determine a more
+        representative range that is not skewed by bright point sources or
+        calibration artefacts.
+    3.  Map data values onto integer colour range [colour_min, 255]. If log is True,
+        we take the log of the data before scaling, and the colour range is scaled
+        to the log of the data range. Missing values are set to their reserved
+        colour index.
     4.  Render the scaled data as an image using matplotlib, with appropriate
         axis labels, title and colourbar. The colourbar tick labels are
         calibrated to show the original data values corresponding to each colour
@@ -52,6 +56,10 @@ def quick_image(
     sigma_clip_level : float, optional
         Number of standard deviations to use as clipping threshold. Default is 3.
         Pass None to skip clipping and use true min/max instead.
+    percentile_clip_level : float, optional
+        Percentile level to use for clipping. For example, a value of 1 means that
+        the display range will be set to the 1st and 99th percentiles of the data.
+        Default is 1. Pass None to skip clipping and use true min/max instead.
     xvals : NDArray[np.floating], optional
         The x-axis values corresponding to the columns of the data array.
     yvals : NDArray[np.floating], optional
@@ -102,22 +110,24 @@ def quick_image(
 
 
     # --- Calculate data range ---
-    # Determine the display range for the data. If not provided, we can apply sigma clipping to the log of the data to determine
+    # Determine the display range for the data. If not provided, we can apply sigma or percentile clipping to the data to determine
     # a more representative range that is not skewed by bright point sources or calibration artefacts.
     # Also calculate and report the percentage of data that is clipped by this process.
-    if data_range is None:
-        positive_data = data[data > 0]  # Creates array containing only positive values from data. This is necessary because we cannot take the log of non-positive values.
-    if len(positive_data) == 0:  # If there are no positive values, we cannot apply logarithmic scaling, so we raise an error.
-        logger.warning(
-            "Data must contain positive values for logarithmic scaling."
-        )
     
-    if sigma_clip_level is not None:  # If sigma clipping is requested
-        log_data = np.log10(positive_data)  # Take log of positive data to get distribution in log space
+    if data_range is None:
+        if log:
+            positive_data = data[data > 0]  # Creates array containing only positive values from data. This is necessary because we cannot take the log of non-positive values.
+            if len(positive_data) == 0:  # If there are no positive values, we cannot apply logarithmic scaling, so we raise an error.
+                logger.warning(
+                   "Data must contain positive values for logarithmic scaling."
+                )
+    
+    if sigma_clip_level is not None and log:  # If sigma clipping is requested
+        data_scaled = np.log10(positive_data)  # Take log of positive data to get distribution in log space
 
         # Use sigma clipping to calculate mean and standard deviation of log data, which will be used to determine clipping bounds.
         # maxiters=5 means we will perform up to 5 iterations of clipping to refine the statistics.
-        mean, median, std = sigma_clipped_stats(log_data, sigma=sigma_clip_level, maxiters=5)  
+        mean, median, std = sigma_clipped_stats(data_scaled, sigma=sigma_clip_level, maxiters=5)  
 
         # Calculate lower and upper bounds of display range in log space, symmetric about the mean. 
         lower_bound = mean - sigma_clip_level * std
@@ -125,8 +135,8 @@ def quick_image(
 
         data_range = [10 ** lower_bound, 10 ** upper_bound]  # Convert bounds back to linear data units (from log space).
         
-        num_clipped = np.sum((log_data < lower_bound) | (log_data > upper_bound))  # Counts how many data points are outside the clipping bounds in log space.
-        percent_clipped = 100 * num_clipped / len(log_data)  # Converts number of clipped points to a percentage of the total data.
+        num_clipped = np.sum((data_scaled < lower_bound) | (data_scaled > upper_bound))  # Counts how many data points are outside the clipping bounds in log space.
+        percent_clipped = 100 * num_clipped / len(data_scaled)  # Converts number of clipped points to a percentage of the total data.
         
         # Report display range and clipping percentage.
         logger.info(
@@ -136,9 +146,27 @@ def quick_image(
             f"Percentage of data clipped: {percent_clipped:.2f}%"
         )
 
-    # No sigma clipping requested, use trye minimum and maximum of the data to determine display range.
-    else: 
-        data_range = [np.min(positive_data), np.max(positive_data)]
+    elif percentile_clip_level is not None and not log:  # If percentile clipping is requested (only applied in linear space)
+        lower_bound = np.nanpercentile(data, percentile_clip_level)  # Calculate lower bound as the value below which a certain percentage of the data falls.
+        upper_bound = np.nanpercentile(data, 100 - percentile_clip_level)  # Calculate upper bound as the value above which a certain percentage of the data falls.
+
+        data_range = [lower_bound, upper_bound]
+
+        num_clipped = np.sum((data < lower_bound) | (data > upper_bound))  # Count how many data points are outside the clipping bounds.
+        percent_clipped = 100 * num_clipped / data.size  # Convert number of clipped points to a percentage of the total data.
+
+        logger.info(
+            f"Percentile clipping of level {percentile_clip_level} applied: {data_range[0]:.2e} to {data_range[1]:.2e}"
+        )
+        logger.info(
+            f"Percentage of data clipped: {percent_clipped:.2f}%"
+        )
+
+    else:  # If no clipping is requested, we can use the full range of the data. If log scaling is applied, we should consider the range of the positive data since non-positive values cannot be displayed on a log scale.
+        if log:
+            data_range = [np.min(positive_data), np.max(positive_data)]  # If applying log scaling, we should only consider the range of the positive data since non-positive values cannot be displayed on a log scale.
+        else:
+            data_range = [np.nanmin(data), np.nanmax(data)] # If not applying log scaling, we can consider the full range of the data including any non-positive values.
         logger.info(
             f"Using full data range: {data_range[0]:.2e} to {data_range[1]:.2e}"
         )
@@ -151,20 +179,26 @@ def quick_image(
     num_colours = colour_max - colour_min + 1
 
     # Calculate log of data range bounds.The lower bound corresponds to colour_min and the upper bound corresponds to colour_max.
-    log_min = np.log10(data_range[0])
-    log_max = np.log10(data_range[1])
+    if log:
+        data_min = np.log10(data_range[0])
+        data_max = np.log10(data_range[1])
 
-    # Take log of data, with non-positive values set to log_min so they land at the bottom of the colour scale.
-    data_log = np.full_like(data, log_min, dtype=float)  # Initalise array for log of data.
-    positive_indices = data > 0  # Create boolean mask for where data is positive.
-    # Take the log of the positive values and leave non-positive values at log_min.
-    data_log[positive_indices] = np.log10(data[positive_indices])
+        # Take log of data, with non-positive values set to data_min so they land at the bottom of the colour scale.
+        data_clipped = np.full_like(data, data_min, dtype=float)  # Initalise array for log of data.
+        positive_indices = data > 0  # Create boolean mask for where data is positive.
+        # Take the log of the positive values and leave non-positive values at data_min.
+        data_clipped[positive_indices] = np.log10(data[positive_indices])
 
-    # Clip log of data to [log_min, log_max] so that any values outside the display range land at the ends of the colour scale rather than wrapping around.
-    data_log = np.clip(data_log, log_min, log_max)
+    else:  # If not applying log scaling, we can skip straight to scaling the data to the colour range.
+        data_clipped = data
+        data_min = data_range[0]
+        data_max = data_range[1]
 
-    # Linearly stretch log of data from [log_min, log_max] onto [colour_min, colour_max] to get array of colour indices corresponding to data values.
-    data_scaled = (data_log - log_min) * num_colours / (log_max - log_min) + colour_min
+    # Clip data to [data_min, data_max] so that any values outside the display range land at the ends of the colour scale rather than wrapping around.
+    data_clipped = np.clip(data_clipped, data_min, data_max)
+
+    # Linearly stretch log of data from [data_min, data_max] onto [colour_min, colour_max] to get array of colour indices corresponding to data values.
+    data_scaled = (data_clipped - data_min) * num_colours / (data_max - data_min) + colour_min
 
     # Set missing pixels to their reserved colour index 0
     if num_missing > 0:
@@ -207,9 +241,10 @@ def quick_image(
     tick_positions = np.linspace(colour_min, colour_max, 5)
 
     # Convert tick positions back to data values using the inverse of the scaling transformation.
-    tick_log_values = (tick_positions - colour_min) / num_colours * (log_max - log_min) + log_min
-    # Take the inverse of the log transformation to get back to linear data values to show on the colourbar ticks.
-    tick_data_values = 10 ** tick_log_values
+    tick_data_values = (tick_positions - colour_min) / num_colours * (data_max - data_min) + data_min
+    if log:
+        # Take the inverse of the log transformation to get back to linear data values to show on the colourbar ticks.
+        tick_data_values = 10 ** tick_data_values
 
     cbar.set_ticks(tick_positions.tolist())  # Set tick positions in colour space
     # Set tick labels to show original data values corresponding to each tick position formatted in scientific notation with 2 significant figures.
