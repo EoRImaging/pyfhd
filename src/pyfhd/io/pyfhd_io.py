97ef5acc2bca92c86f87decbfb1cf8be62340a4d
Bryna Hazelton <bryna.hazelton@gmail.com>
2026-09-10 09:32:19 -0700

    More testing for save/load

--- src/pyfhd/io/pyfhd_io.py
+++ src/pyfhd/io/pyfhd_io.py
@@ -7,6 +7,7 @@ import h5py
 import numpy as np
 import yaml
 from numpy.typing import NDArray, DTypeLike
+from pyuvdata.analytic_beam import AnalyticBeam
 from scipy.io import readsav
 from scipy.sparse import csr_array
 
@@ -315,7 +316,7 @@ def save_dataset(
             group = h5py_obj.create_group(key)
             dict_to_group(group, csr_attrs, to_chunk, variable_lengths)
         case _:
-            if isinstance(key, str) and "analytic_beam" in key:
+            if isinstance(key, str) and isinstance(value, AnalyticBeam):
                 yaml_str = yaml.safe_dump(value)
                 h5py_obj.create_dataset(key, data=np.bytes_(yaml_str))
             else:
@@ -512,6 +513,8 @@ def load_dataset(
             value = _decode_byte_arr(value)
         if isinstance(value, bytes):
             value = value.decode()
+            if value.startswith("!AnalyticBeam"):
+                value = yaml.safe_load(value)
         return value
 
 
